7c0ab05de20291c704774aff15cebbd1071bec1b
Nazz <73013033+nazz2000@users.noreply.github.com>
2024-12-16 01:06:36 +0200

    Delete Proect-main/poect directory

--- Proect-main/poect/managingsystem/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Appliance, PowerCalculation, PowerConsumption
-
-admin.site.register(Appliance)
-admin.site.register(PowerCalculation)
-admin.site.register(PowerConsumption)
